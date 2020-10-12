9953f599cd0df6bcd2253dac048815f7576f9f7b
Raj Nandigalla <raj.nandigalla@ril.com>
2020-10-13 03:01:18 +0530

    base url interceptor added

--- src/packages/interceptors/BaseURLInterceptor.ts
+++ src/packages/interceptors/BaseURLInterceptor.ts
@@ -1,12 +1,15 @@
-
 import { Observable } from 'rxjs/internal/Observable';
 import { HttpRequest } from '../core/request';
 import { HttpHandler } from '../core/backend';
 import { HttpEvent } from '../core/response';
+import { elixorConfig } from './../index';
 
 /**
  *  @publicApi
  */
 export const BaseURLInterceptor = (req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> => {
+    if (!(req.url && new RegExp('^(http[s?://]+)').test(req.url)))
+        if (elixorConfig.baseURL)
+            req = req.clone({ url: elixorConfig.baseURL + req.url });
     return next.handle(req);
 };
